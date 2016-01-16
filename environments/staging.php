fbbd2e7600a72f385c4182216c22fdf517dbd655
Paul Graham <pgraham@hlkagency.com>
2016-01-16 02:05:56 -0600

    build: adjusts security settings on environment configs

--- environments/staging.php
+++ environments/staging.php
@@ -1,6 +1,6 @@
 <?php
 /* Staging */
+define('FORCE_SSL_ADMIN', true);
 ini_set('display_errors', 0);
 define('WP_DEBUG_DISPLAY', false);
 define('SCRIPT_DEBUG', false);
-define('DISALLOW_FILE_MODS', true); // this disables all file modifications including updates and update notifications
